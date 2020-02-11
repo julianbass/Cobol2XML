160dc63b1f2c48f1087da06948061a9aaba17fbc
SGB807 <omarfarrah2000@outlook.com>
2020-02-11 12:27:19 +0000

    Added a tokenizer test class

---
+++ Cobol2XML/src/AllTests.java
@@ -0,0 +1,9 @@
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DivisionTests.class, Program_IDTests.class, SecctionTests.class, TokeinzerTests.class })
+public class AllTests {
+
+}
---
+++ Cobol2XML/src/TokeinzerTests.java
@@ -0,0 +1,33 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import cobol.Cobol;
+import cobol.CobolParser;
+import parse.Assembly;
+import parse.Parser;
+import parse.tokens.TokenAssembly;
+import parse.tokens.Tokenizer;
+import parse.tokens.WhitespaceState;
+
+public class TokeinzerTests {
+
+	@Test
+	public void testTokenizer() {
+		Tokenizer t = CobolParser.tokenizer();
+		Parser p = CobolParser.start();
+		
+		
+		t.setCharacterState(   0,   ' ', new WhitespaceState());
+		
+		t.setString("program-id. base_jb12");
+		Assembly in = new TokenAssembly(t);
+		Assembly out = p.bestMatch(in);
+	
+		Cobol c = new Cobol();
+		c = (Cobol) out.getTarget();
+		
+		assertEquals(c.getProgram_ID(), "base_jb12");
+	}
+
+}
