e1d3318983bf930292335847d9898a1d6447ca97
Julian Bass <j.bass@salford.ac.uk>
2021-02-16 13:45:38 +0000

    Disguise comment out source code

--- Cobol2XML/src/cobol/Cobol2XML.java
+++ Cobol2XML/src/cobol/Cobol2XML.java
@@ -51,10 +51,10 @@ public class Cobol2XML {
 		 * In case you are not sure if you are pointing toward the right file, print out the filename
 		 * like this...
 		 *
-		 * System.out.println("arg[0]" + args[0]);
+		 * System.out.println("arg[0]" + args[0])
 		 * 
 		 * A rather crude approach is to hard code the filename for the cobol source file, like this
-		 * InputStream is = new FileInputStream("C:\\Users\\sgs442\\eclipse-workspace\\CobolParser1\\base.cbl");
+		 * InputStream is = new FileInputStream("C:\\Users\\sgs442\\eclipse-workspace\\CobolParser1\\base.cbl")
 		 */
 
 		InputStream is = new FileInputStream(args[0]);
